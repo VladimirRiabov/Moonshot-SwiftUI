9024e8c89d29847b90e4b10897504369ce5bb800
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-14 15:54:47 +0300

    How to lay out views in a scrolling grid – Moonshot SwiftUI Tutorial 5/11

--- Moonshot SwiftUI/ContentView.swift
+++ Moonshot SwiftUI/ContentView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 
 struct ContentView: View {
+    let layout = [
+        //grid - сетка
+        GridItem(.adaptive(minimum: 80))
+    ]
+    
     var body: some View {
-        NavigationView {
-            List(0..<100) { row in
-                NavigationLink {
-                    //содержимое открывающегося navigationView
-                    Text("NavigationView\(row)")
-                } label: {
-                    //название строки-кнопки
-                    Text("Navigation link \(row)")
+        
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(0..<1000) {
+                    Text("Text\($0)")
                 }
-                .navigationTitle("First view")
             }
         }
+       
         
     }
 }
