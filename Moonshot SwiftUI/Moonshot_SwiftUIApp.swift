3e96b2b7d1ea7b1bf2d77deee8ad19a8d91728ed
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-14 12:15:37 +0300

    Initial Commit

---
+++ Moonshot SwiftUI/Moonshot_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_SwiftUIApp.swift
+//  Moonshot SwiftUI
+//
+//  Created by Владимир Рябов on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
