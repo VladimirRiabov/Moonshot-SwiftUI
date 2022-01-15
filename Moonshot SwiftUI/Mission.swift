6618a5728253b365340427d80ec4061547464c31
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-15 14:58:56 +0300

    Formatting our mission view – Moonshot SwiftUI Tutorial 8/11

--- Moonshot SwiftUI/Mission.swift
+++ Moonshot SwiftUI/Mission.swift
@@ -7,14 +7,29 @@
 
 import Foundation
 
-struct CrewRole: Codable {
-    let name: String
-    let role: String
-}
+
 
 struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
 }
